75571701b4cd7ca1df4b43342b93296756c4c3e2
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-09-27 03:50:51 +0530

    Add files via upload
    variables in java

---
+++ variables.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String args[]){
+        String Fname="Aravind"; // use double quote for string
+        String Lname="Kumar";
+        int age = 21;
+        float percentage=71.8f; // use f with float value
+        char gender='M'; // use single quote for char
+        boolean result=true;
+        System.out.println("My name is "+Fname+Lname);
+        System.out.println("My age is "+age);
+        System.out.println("My percentage is "+percentage);
+        System.out.println("Gender is "+gender);
+        System.out.println("result is "+result);
+    }
+}
