ba1e2c968ac82548aafcbb9ba23e9e0553fa3021
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-09-27 05:08:44 +0530

    Relational operators

---
+++ relational.java
@@ -0,0 +1,10 @@
+ public class Main{
+    public static void main(String args[]){
+        int a=10,b=5;
+        System.out.println("Equal "+(a==b));
+        System.out.println("Not Equal "+(a!=b));
+        System.out.println("Greater than or equal to "+(a>=b));
+        System.out.println("Less than or equal to "+(a<=b));
+       
+    }
+}
