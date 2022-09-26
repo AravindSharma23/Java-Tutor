890e6b8bdbe88ea3f191afac4453d665a7618ac2
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-09-27 04:50:30 +0530

    Arithmetic operator

---
+++ arithematic.java
@@ -0,0 +1,18 @@
+//Arithmetic operators
+public class Main{
+    public static void main(String args[]){
+        int a=5,b=10;
+        System.out.println("Addition "+(a+b)); 
+        System.out.println("Add 10 with it  "+(a+b+10));
+        // int add=a+b;
+        // int sub=a-b;
+        // int mul=a*b;
+        // int div=a/b;
+        // int modulo = a%b;
+    //     System.out.println("Addition"+add);
+    //     System.out.println("subtrction"+sub);
+    //     System.out.println("multuplication"+mul);
+    //     System.out.println("division"+div);
+    //     System.out.println("modulo"+modulo);
+    }
+}
