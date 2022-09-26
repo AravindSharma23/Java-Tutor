2140b43c5b07d212b479720f2abf01be714919e5
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-09-27 04:17:51 +0530

    Type conversion

--- type_conv.java
+++ type_conv.java
@@ -14,7 +14,7 @@ public class Main{
         int a=23;
         float b = 99.9f;
         int mark = 431;
-        float tot=(float)mark;
+        float tot=(float)mark; // when type casting-----> (Converting data type)existing variable_name
         System.out.println("Marks in float "+tot);
         System.out.println("Double val is "+(double)b);
         System.out.println("float val of a "+(float)a);
