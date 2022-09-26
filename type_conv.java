64abb7adb77359623fc8a04e3847665864e21929
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-09-27 04:15:13 +0530

    Add files via upload
    Type conversion in java

---
+++ type_conv.java
@@ -0,0 +1,24 @@
+/*
+Widening casting :
+      byte->short->char->int->long->float->Double
+      
+Narrowinf casting :
+     double->float->long->int->char->short->byte
+*/
+
+
+
+
+public class Main{
+    public static void main(String args[]){
+        int a=23;
+        float b = 99.9f;
+        int mark = 431;
+        float tot=(float)mark;
+        System.out.println("Marks in float "+tot);
+        System.out.println("Double val is "+(double)b);
+        System.out.println("float val of a "+(float)a);
+        
+        
+    }
+}
