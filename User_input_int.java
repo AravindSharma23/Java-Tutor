2644bd3bc6919e2c1920ec3cdc138f7342edc481
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-09-27 10:35:30 +0530

    user input in int type

---
+++ User_input_int.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter two vals:");
+        a=in.nextInt();
+        b=in.nextInt();
+        c=a+b;
+        System.out.println("Total is "+c);
+        
+    }
+}
