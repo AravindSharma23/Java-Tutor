ec16bbac4c07c0ecb176b4dfcc183bf0f9386fcc
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-09-27 10:44:31 +0530

    User input in float data type

---
+++ user_input_float.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+public static void main (String[] args) {
+   Scanner in = new Scanner(System.in);
+   float x,y,z;
+   x=in.nextFloat();
+   y=in.nextFloat();
+   z=x*y;
+   System.out.println("The multiplied value is "+z);
+}
+}
