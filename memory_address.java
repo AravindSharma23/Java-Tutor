c56ff2bbad75e549575dd59258d7ce16740092d1
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-10-04 03:57:56 +0530

    To get Memory address of values

---
+++ memory_address.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	String a = "Aravind";
+	String b = "Aravind";
+	String c = "Kumar";
+	System.out.println("A memory address:"+a.hashCode());
+	System.out.println("B memory address:"+b.hashCode());
+	System.out.println("C memory address:"+c.hashCode());
+	}
+}
+
