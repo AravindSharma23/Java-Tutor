c54e53b88edc5b768c82626943acfd24c847dff0
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-10-05 04:14:37 +0530

    character array

---
+++ character_arr.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	String a = "Aravind kumar";
+    char[] char_arr = a.toCharArray();
+	for(char ca:char_arr){
+	    SYstem.out.println(ca);
+	}
+	}
+}
+
