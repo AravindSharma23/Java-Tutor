6f42464e52b8d0bede6cdb3977e6acfd7b56f2c7
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2022-10-06 12:53:34 +0530

    Different map collections in java

---
+++ MapCollections.java
@@ -0,0 +1,20 @@
+//HashMap
+       //It contains value based on key.
+       //Contains unique elements.
+       //Maintain no order.
+       //One null key many null value.
+       
+       
+//TreeMap
+    //It contains value based on key.
+    //Contains unique elements.
+    //Maintain Ascending order.
+    //No null key many null value.
+    
+    
+//Linked HashMap
+      //It contains value based on key.
+       //Contains unique elements.
+       //Maintain order.
+       //One null key many null value.
+       
