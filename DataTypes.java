d7c8810f2d4a70c8120336b89aaafb26725f1ebd
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-03-29 10:08:49 +0530

    Primitive datatypes ranges

---
+++ DataTypes.java
@@ -0,0 +1,28 @@
+public class Main
+{
+	public static void main(String[] args) {
+	
+/*
+   DataType(Whole Number)           Range
+1. byte                          -128,127
+2. short                         -32k,-32k
+3. int                           -2billion,2billion
+4. long                          -2.14 billion to 2.14 billion
+*/ 	
+
+/*
+  DataType	 Size	                 Description
+byte	      1 byte	           Stores whole numbers from -128 to 127
+short	      2 bytes        	   Stores whole numbers from -32,768 to 32,767
+int	          4 bytes	           Stores whole numbers from -2,147,483,648 to 2,147,483,647
+long          8 bytes	           Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+float	      4 bytes	           Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+double   	  8 bytes	           Stores fractional numbers. Sufficient for storing 15 decimal digits
+boolean	      1 bit	               Stores true or false values
+char	      2 bytes	           Stores a single character/letter or ASCII values
+*/ 	
+
+
+	
+	}
+}
