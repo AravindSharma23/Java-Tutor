e6572ba75c82f61fc8bfae45c96d076cabc54cb9
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-04-20 05:37:01 +0530

    2d_array.java

--- 2d_array.java
+++ 2d_array.java
@@ -4,17 +4,32 @@ class Main{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[][]=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j =0;j<n;j++){
-                arr[i][j] = sc.nextInt();
+        for(int row=0;row<n;row++){
+            for(int col =0;col<arr[row].length;col++){
+                arr[row][col] = sc.nextInt();
             }
             
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(" "+arr[i][j]);
-            }
-            System.out.println("");
+    
+         
+    for(int row=0;row<n;row++){
+        for(int col=0;col<arr[row].length;col++){
+        System.out.print(arr[row][col]+" ");
         }
+    
+    System.out.println();
+	    
+	}
+	
+	/*
+	for(int row=0;row<arr.length;row++){
+	    System.out.print(Arrays.toString(arr[row]));
+	}
+	*/
+    /*
+	for(int[] a:arr){ // Enhanced for loop
+	    System.out.println(Arrays.toString(a));
+	}
+        */
     }
 }
