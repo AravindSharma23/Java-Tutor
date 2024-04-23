3b4ac771f031a47d8792b4d839e9516dd5938c43
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-04-24 04:59:53 +0530

    To find no of even digits in an array

--- DSA Based Problems/LinearSearch Based Problems/7_NoOfEvenDigits.java
+++ DSA Based Problems/LinearSearch Based Problems/7_NoOfEvenDigits.java
@@ -6,14 +6,23 @@ public class Main
 		int[] arr= {12,345,2,6,7896};
 		System.out.println("No of even digits numbers "+checkNumbers(arr));
 	}
-	static int digits(int num){
-	    int count = 0;
-	    while(num >0){
-	        count++;
-	         num/=10;
-	    }
-	    return count;
+		static int digits(int num){ // optimised way to find no of digits
+	           return  (int) Math.log10(num)+1;  
 	}
+	// static int digits(int num){
+	//     if(num < 0){
+	// 	    return num = num * -1;
+	//     }
+	//     if(num == 0){
+	// 	    return -1;
+	//     }
+	//     int count = 0;
+	//     while(num >0){
+	//         count++;
+	//          num/=10;
+	//     }
+	//     return count;
+	// }
 	static boolean isEven(int num){
 	    int digitsCount = digits(num);
 	    return  digitsCount%2 == 0;
