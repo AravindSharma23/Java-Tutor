8c37b71bf303e22dcfd41404cc400a4a41f8098d
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-04-28 21:15:55 +0530

    Order Agnostic Array in Ascending & Descending arr

--- DSA Based Problems/BinarySearchBasedProblems/3_OrderAgnosticArray.java
+++ DSA Based Problems/BinarySearchBasedProblems/3_OrderAgnosticArray.java
@@ -1,2 +1,48 @@
 // Order Agnostic  Array 
 
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	int[] arr = {90,75,18,12,6,4,3,1}; // sorted Array
+
+	int target = 4;
+	int ans = agrosticBinarySearch(arr,target);
+	System.out.print("Result is "+ans);
+	}
+	static int agrosticBinarySearch(int[] arr, int target){
+	    int start = 0;
+	    int end = arr.length-1;
+	    boolean isAscending = arr[start]<arr[end];
+	    
+	    while(start<=end){
+	        // int mid = (start+end)/2 -> It makes error bcz it exceeds Integer limit.so use below for mid. 
+	        int mid = start+(end-start)/2;
+	        if(target == arr[mid]){
+	             return mid;
+	        }  
+	        if(isAscending){   // For Ascending ordered array
+	            if(target < arr[mid]){
+	                end = mid-1;
+	            }else{
+	                start = mid+1;
+	            }
+	        }
+	        else{             // For Descending ordered array
+	            if(target < arr[mid]){
+	                start = mid+1;
+	            }else{
+	                end = mid-1;
+	            }
+	        }
+	       
+	        
+	    }
+	      return -1;
+	    }
+     }
+	
+
+
+
