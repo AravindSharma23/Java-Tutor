e4c759d69b06f927eef38c3ae4438ea644bbb5c2
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-05-12 21:11:59 +0530

    Floor Of Number in Array

---
+++ DSA Based Problems/BinarySearchBasedProblems/5_FloorOfNumber.java
@@ -0,0 +1,31 @@
+// As a same as binary search 
+// largest number that is less than or equal to target.
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[] = {2,3,5,9,14,16,18};
+		int target = 15;
+		int res = floor(arr,target);
+		System.out.print("Res is "+res);
+	}
+	static int floor(int[] arr,int target){
+	    int start = 0;
+	    int end = arr.length-1;
+	   
+	    while(start<=end){
+	        int mid = start+(end-start)/2;
+	        if(arr[mid]>target){
+	            end = mid-1;
+	        }
+	        else if(arr[mid]<target){
+	            start = mid+1;
+	        }else{
+	            return mid;
+	        }
+	    }
+	    return end;
+	}
+}
+
