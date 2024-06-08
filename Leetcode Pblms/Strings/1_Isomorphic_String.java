78fdc610f931c758d099fba2eb1add7f03e25761
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-06-09 00:18:09 +0530

    Isomorphic String - Easy

---
+++ Leetcode Pblms/Strings/1_Isomorphic_String.java
@@ -0,0 +1,20 @@
+// problem no : 205 ->  Easy- > Isomorphic String
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+     HashMap<Character,Character> map = new HashMap<Character,Character>();
+     for(int i=0;i<s.length();i++){
+        if(map.containsKey(s.charAt(i))){
+            if(!map.get(s.charAt(i)).equals(t.charAt(i))){
+                return false;
+            }
+        }else{
+            if(map.containsValue(t.charAt(i))){
+                return false;
+            }
+            map.put(s.charAt(i),t.charAt(i));
+        }
+     }
+     return true;
+    }
+}
