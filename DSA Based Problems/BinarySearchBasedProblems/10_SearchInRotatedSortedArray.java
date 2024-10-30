bec10a5ab57bab89e3e3c7f7d159e4eb173b5a88
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-10-30 11:26:37 +0530

    Search in Rotated Sorted Array

--- DSA Based Problems/BinarySearchBasedProblems/10_SearchInRotatedSortedArray.java
+++ DSA Based Problems/BinarySearchBasedProblems/10_SearchInRotatedSortedArray.java
@@ -3,33 +3,45 @@
 
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+         return result(nums,target);
+    }
+  	static int findPivot(int[] arr){  // step 1 -> Find pivot
+        int max = 0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[max]< arr[i]){
+                max = i;
+            }else{
+                break;
+            }
+        }
+        return max;
+    }
+    static int result(int[] arr,int target){
+        int idx = -1;
+        if(arr[0]>arr[arr.length-1]){  // To check whether array is rotationally sorted
+            int maxVal = findPivot(arr);
 
-        while (start <= end) {
-            // Finding the mid using floor division
-            int mid = start + (end - start) / 2;
+            if(target > arr[arr.length-1]){    // check for target in 1st part of array
+               idx =  binarySearch(arr,0,maxVal,target);
 
-            // Target value is present at the middle of the array
-            if (nums[mid] == target) {
+            }else{           // check for target in 2nd part of array
+           idx =  binarySearch(arr,maxVal+1,arr.length-1,target);
+
+        }
+          return idx;   
+        }
+        return binarySearch(arr,0,arr.length-1,target);   // To check for target in a sorted array
+    }
+    static int binarySearch(int[] arr,int start,int end,int target){
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid] == target){
                 return mid;
             }
-
-            // start to mid is sorted
-            if (nums[start] <= nums[mid]) {
-                if (nums[start] <= target && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-            // mid to end is sorted
-            else {
-                if (nums[mid] < target && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+            if(arr[mid]>target){
+                end = mid-1;
+            }else if(arr[mid] < target){
+                start = mid+1;
             }
         }
         return -1;
