8d932da4a5f13c275dfce609011a6d0ce7d043db
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-12-21 21:18:29 +0530

    Missing Number-amazon-leetcode

---
+++ DSA Based Problems/Sortings/Cyclic Sort/1_MissingNumber.java
@@ -0,0 +1,41 @@
+/*
+
+Leetcode -> 268 -> Easy - Amazon question.
+*/
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = {3,0,1}; 
+	    int i = 0;
+	    while(i < arr.length){
+	        int correct = arr[i];
+	        if(arr[i] < arr.length && arr[i] != arr[correct]){
+	            swap(arr,i,correct);
+	        }else{
+	            i++;
+	        }
+	    }
+	   
+	  System.out.println("Array is "+Arrays.toString(arr));
+	  System.out.println("Missing num is "+findMissingNumber(arr));
+      
+	}
+   static void swap(int[] arr,int first,int second){
+       int temp = arr[first];
+       arr[first] = arr[second];
+       arr[second] = temp;
+   }
+   static int findMissingNumber(int[] arr){
+        for(int index=0;index<arr.length;index++){
+	        if(arr[index] != index){ // case1 -> return missed num
+	            return index;
+	        }
+	    }
+	    return arr.length; // return lastly missed num
+
+   }
+    
+}
