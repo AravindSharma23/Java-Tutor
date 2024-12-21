ae62dfae5c790f2a8192d3b83204ca28a8dcfc01
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-12-21 09:16:24 +0530

    Insertion Sort

---
+++ DSA Based Problems/Sortings/InsertionSort.java
@@ -0,0 +1,34 @@
+/*
+Insertion sort :
+  -> It works good in partially sorted array(Hybrid array).
+  -> stable
+  
+*/
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = {2,5,1,0,-3,7};
+	   	   //int[] arr = {};
+
+	    for(int i=1;i<arr.length;i++){
+	        for(int j=i;j>0;j--){
+	            if(arr[j]<arr[j-1]){
+	                swap(arr,j,j-1);
+	            }
+	            else{
+	                break;
+	            }
+	        }
+	    }
+	    System.out.println("arr is "+Arrays.toString(arr));
+	}
+
+	static void swap(int[] arr,int first,int second){
+	    int temp = arr[first];
+	     arr[first] = arr[second];
+	    arr[second] = temp;
+	}
+}
