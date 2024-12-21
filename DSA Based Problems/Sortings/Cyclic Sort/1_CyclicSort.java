999c60801dd260bb46555b5d4c7af82a20c224c0
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-12-21 19:36:00 +0530

    Cyclic Sort

---
+++ DSA Based Problems/Sortings/Cyclic Sort/1_CyclicSort.java
@@ -0,0 +1,32 @@
+/*
+Cyclic sort :
+ -> Ranges from 1 to N use cyclic sort.
+ 
+*/
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = {3,5,2,1,4}; 
+	    int i = 0;
+	    while(i<arr.length){
+	        int check = arr[i]-1;
+	        if(arr[i] != arr[check]){
+	            swap(arr,i,check);
+	        }else{
+	            i++;
+	        }
+	    }
+	    
+	  System.out.println("Array is "+Arrays.toString(arr));
+      
+	}
+   static void swap(int[] arr,int first,int second){
+       int temp = arr[first];
+       arr[first] = arr[second];
+       arr[second] = temp;
+   }
+    
+}
