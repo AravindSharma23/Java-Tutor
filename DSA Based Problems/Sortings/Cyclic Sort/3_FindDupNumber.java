2ed501bef674acddf719ca200099715e249d48e1
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-12-23 12:45:15 +0530

    Find Duplicate Number -leetcode-amazon - easy

---
+++ DSA Based Problems/Sortings/Cyclic Sort/3_FindDupNumber.java
@@ -0,0 +1,30 @@
+/*
+Find Duplicate Number -> 287 - leetcode - >Aslked in  Amazon. -> Easy
+
+*/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
+            }
+        }
+        int repeatedNum = 0;
+        for(int index=0;index<nums.length;index++){
+            if(nums[index] != index+1){
+                repeatedNum =  nums[index];
+            }
+        }
+        return repeatedNum;
+    }
+    static void swap(int[] nums,int first,int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
