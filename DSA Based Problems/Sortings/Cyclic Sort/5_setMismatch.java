488a472b43ced75b90568d5d46802068f2c76067
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2024-12-23 13:34:33 +0530

    Set Mismatch -leetcode-easy

---
+++ DSA Based Problems/Sortings/Cyclic Sort/5_setMismatch.java
@@ -0,0 +1,30 @@
+/*
+Set Mismatch - 645 - leetcode - easy
+*/
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] res = new int[2]; 
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
+            }
+        }
+       for(int index=0;index<nums.length;index++){
+        if(nums[index] != index+1){
+            res[0] = nums[index];
+            res[1] = index+1;
+        }
+       }
+       return res;
+    }
+    static void swap(int[] nums,int first,int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
