cbe7b3676a7562c8b7c06732e0d3088fe330f220
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-02-20 10:25:43 +0530

    Contains Duplicates - Easy - Important

---
+++ Leetcode Pblms/6_containsDuplicates.java
@@ -0,0 +1,14 @@
+//217 - >  Contains Duplicates - Easy - Important
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int n : nums){
+            if(set.contains(n)){
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
