4251882f09d99740e13d3a96addad1567f5738b0
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-03-24 04:59:10 +0530

    LinkedList cycle  - 141 - Leetcode -easy

---
+++ DSA Based Problems/LinkedList/2_LinkedListCycle.java
@@ -0,0 +1,19 @@
+// LinkedList cycle  - 141 - Leetcode -easy
+/* 
+Its a fast and slow pointer method - pattern for solve a pblm.
+F moves 2steps at a time but s moves one step at a time.
+*/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+             return true;
+            }
+        }
+        return false;
+    }
+}
