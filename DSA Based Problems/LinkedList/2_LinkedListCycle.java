455228efc993b4ebe06ac9d0470f7ae7620b368d
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-03-26 05:01:20 +0530

    LinkedList cycle  - 141 - Leetcode -easy

--- DSA Based Problems/LinkedList/2_LinkedListCycle.java
+++ DSA Based Problems/LinkedList/2_LinkedListCycle.java
@@ -7,7 +7,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-        while(fast != null && fast.next != null){
+        while(fast != null && fast.next != null){ // why we add fast and fast.next != null means if cycle loop means there is no null.
             fast = fast.next.next;
             slow = slow.next;
             if(fast == slow){
