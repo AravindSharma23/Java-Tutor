7e88d59d22089b12d4ff574686c1c43e0298da79
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-03-30 22:15:30 +0530

    Middle of linked list - leetcode -easy

---
+++ DSA Based Problems/LinkedList/7_MiddleOfLinkedList.java
@@ -0,0 +1,13 @@
+// Middle of linked list - 876 - leetcode -easy
+// Used Two pointers method(hare and tortoise Algorithm)
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
