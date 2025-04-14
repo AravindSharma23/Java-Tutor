40f62e1c071f7f7d1f04c6232ea1bd65fb30d50d
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-04-15 04:03:50 +0530

    Deque Implementation

---
+++ DSA Based Problems/Stack & Queue/DequeImplementation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(4);
+        deque.addLast(7);
+        System.out.println(deque.removeFirst());
+        
+
+
+    }
+}
