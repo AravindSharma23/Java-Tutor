460c6b94c5417c63ac176995ecf94909d5d08504
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-08-18 15:36:20 +0530

    Merge two sorted linked lists - Optimized using recursion

---
+++ GFG Pblms/GFG 160/LinkedList Pblms/2_Merge2SortedLinkedList.java
@@ -0,0 +1,33 @@
+// Merge two sorted linked lists - Optimized 
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+*/
+
+class Solution {
+    Node sortedMerge(Node head1, Node head2) {
+        // code here
+       if(head1 ==null){
+           return head2;
+       }
+       if(head2 == null){
+           return head1;
+       }
+       if(head1.data <= head2.data){
+           head1.next = sortedMerge(head1.next , head2);
+           return head1;
+       }else{
+            head2.next = sortedMerge(head2.next , head1);
+           return head2;
+       }
+       
+       
+    }
+}
+
+
