231769f485f8cd16dfe2ae8ddd7cf73b3b12f630
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-09-30 21:23:59 +0530

    Permutation  Using recursion and return ArrayList

---
+++ DSA Based Problems/Recursion/Strings/10_PermutationReturnArraylist.java
@@ -0,0 +1,27 @@
+// Permutation  Using recursion and return ArrayList
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	 System.out.println(permutation("","abc")); 
+	}
+	static ArrayList<String> permutation(String p,String up){
+	    if(up.isEmpty()){
+	        ArrayList<String> list = new ArrayList<>();
+	        list.add(p);
+	        return list;
+	    }
+	    char ch = up.charAt(0);
+	    ArrayList<String> ans = new ArrayList<>();
+	    for(int i = 0;i<=p.length();i++){
+	        String f = p.substring(0,i);
+	        String s = p.substring(i,p.length());
+
+	        
+	       ans.addAll(permutation(f+ch+s,up.substring(1)));
+	    }
+	    return ans;
+	}
+}
+
+
