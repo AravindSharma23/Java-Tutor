7be9a029bf38f048044ba092b38c44bcd2b94a0d
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2025-09-30 16:04:20 +0530

    Permutation using recursion in string

--- DSA Based Problems/Recursion/Strings/9_Permutation.java
+++ DSA Based Problems/Recursion/Strings/9_Permutation.java
@@ -23,3 +23,31 @@ public class Main
 	    }
 	}
 }
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	    String s = "abc";
+// 	  permutation(s.toCharArray(),0);
+// 	}
+// 	static void swap(char[] arr,int i , int fi){
+// 	    char temp = arr[i];
+// 	    arr[i] = arr[fi];
+// 	    arr[fi] = temp;
+	    
+// 	}
+// 	static void permutation(char[] arr,int fi){
+// 	   if(fi == arr.length-1){
+// 	       System.out.println(Arrays.toString(arr));
+// 	       return;
+// 	   }
+// 	   for(int i = fi;i<arr.length;i++){
+// 	       //System.out.println(fi);
+// 	       swap(arr,i,fi);
+// 	       permutation(arr,fi+1);
+// 	       swap(arr,i,fi);
+
+// 	   }
+// 	}
+}
