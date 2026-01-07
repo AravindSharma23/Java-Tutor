75d2574a7dd9002c83f242b71f8267c9f170b438
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2026-01-07 10:26:08 +0530

    Sqrt(x) - leetcode - 69 - easy- IMP

---
+++ DSA Based Problems/BinarySearchBasedProblems/13_SquareRootOfNum.java
@@ -0,0 +1,19 @@
+// Sqrt(x) - leetcode - 69 - easy- IMP
+
+// REFER Striver youtube 
+class Solution {
+    public int mySqrt(int x) {
+        int low = 1;
+        int high = x;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            long val = (long)mid*mid;
+            if(val <= x){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return high;
+    }
+}
