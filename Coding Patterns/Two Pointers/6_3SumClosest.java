5ad97687f12c28ae70b5d0683e83352a30eeb81c
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2026-08-15 09:31:02 +0530

    Leetcode  16 - 3 sum closest - Medium - Imp

---
+++ Coding Patterns/Two Pointers/6_3SumClosest.java
@@ -0,0 +1,33 @@
+// Leetcode  16 - 3 sum closest - Medium - Imp
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        //if(nums == null || nums.length<3) return 0;
+        Arrays.sort(nums);
+        int resSum = nums[0]+nums[1]+nums[2];
+        int minDiff = Integer.MAX_VALUE;
+
+        for(int i = 0;i<nums.length-2;i++){
+            int left = i+1;
+            int right = nums.length-1;
+
+            while(left<right){
+             int sum = nums[i]+nums[left]+nums[right];
+                if(sum ==target){
+                    return sum;
+                }else if(sum < target){
+                    left++;
+                }else{
+                    right--;
+                }
+                // Find diff btw target & sum bcz to find min val(closest)
+                int targetDifference = Math.abs(sum-target);
+                if(targetDifference < minDiff){
+                    resSum = sum;
+                    minDiff = targetDifference;
+                }
+            }
+        }
+        return resSum;
+    }
+}
