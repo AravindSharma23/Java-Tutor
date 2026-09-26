77e273310c211be515821a473562b9a51710e3a4
Aravindkumar <86199183+AravindSharma23@users.noreply.github.com>
2026-09-26 21:52:32 +0530

    Leetcode - Medium -  1541. Minimum Insertions to Balance a Parentheses String

--- DSA Based Problems/Stack & Queue/5_MinimumInsertionToBalanceParenthesisString.java
+++ DSA Based Problems/Stack & Queue/5_MinimumInsertionToBalanceParenthesisString.java
@@ -24,3 +24,31 @@ class Solution {
         return count+=2*stack.size();
     }
 }
+
+// more optimized solution :
+
+class Solution {
+    public int minInsertions(String s) {
+        int openBracket = 0,closeBracket = 0;
+
+        for(int i = 0;i<s.length();i++){
+           char c = s.charAt(i);
+           if(c == '('){
+            openBracket++;
+           }else{
+            if(i+1 < s.length() && s.charAt(i+1)== ')'){
+                i++;
+            }else{
+                closeBracket++;
+            }
+            if(openBracket > 0){
+               openBracket--;
+            }else{
+                closeBracket++;
+            }
+           }
+        }
+                   return closeBracket+(openBracket*2);
+
+    }
+}
